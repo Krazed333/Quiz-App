d5fff91cd3f4a7ee4b1c7e0ff5999f28a00fef5f
Kamal <kkayramos3@gatech.edu>
2024-03-09 19:30:41 -0500

    Refactor CSS styles and fix formatting issues

--- src/Pages/Results/Results.js
+++ src/Pages/Results/Results.js
@@ -1,32 +1,30 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Button } from '@mui/material';
-import "./Results.css"
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import "./Results.css";
 
-const Results = ({name,score}) => {
+const Results = ({ name, score }) => {
   const navigate = useNavigate();
 
-
-  useEffect(()=>{
-    if(!name){
+  useEffect(() => {
+    if (!name) {
       // navigate("/")
     }
-
-  },[name,navigate])
-  return(
-   <div className="result">
-  <span className="title">Final Score : {score}</span>
-  <Button
-    variant="contained"
-    color="secondary"
-    size="large"
-    style={{ alignSelf: "center", marginTop: 20 }}
-    href="/"
-  >
-    Go to homepage
-  </Button>
-</div>
-);
+  }, [name, navigate]);
+  return (
+    <div className="result">
+      <span className="title">Final Score : {score}</span>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        style={{ alignSelf: "center", marginTop: 20 }}
+        href="/"
+      >
+        Go to homepage
+      </Button>
+    </div>
+  );
 };
 
-export default Results
+export default Results;
